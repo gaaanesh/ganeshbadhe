3f0e94abe875d21221eb9381cc23af144a11ecc4
ganeshbadhe5 <ganesh.badhe5@gmail.com>
2025-12-03 10:14:06 +0530

    added gtag only

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -53,14 +53,14 @@ export const metadata: Metadata = {
 };
 
 // const googleAnalyticsId = process.env.GOOGLE_ANALYTICS_ID;
-const googleAnalyticsId = process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID;
+const googleAnalyticsId = process.env.NEXT_PUBLIC_GA_ID;
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en" className="!scroll-smooth" suppressHydrationWarning>
       {googleAnalyticsId && (
         <head>
-          {/* <Script async src={`https://www.googletagmanager.com/gtag/js?id=${googleAnalyticsId}`} />
+          <Script async src={`https://www.googletagmanager.com/gtag/js?id=${googleAnalyticsId}`} />
           <Script id="google-anayltics-script">
             {`
               window.dataLayer = window.dataLayer || [];
@@ -69,15 +69,6 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
               gtag('config', '${googleAnalyticsId}');
             `}
           </Script>
-          <!-- Google tag (gtag.js) --> */}
-    <script async src="https://www.googletagmanager.com/gtag/js?id=G-X2MEN4YWXH"></script>
-    <script>
-      {`window.dataLayer = window.dataLayer || [];
-      function gtag(){dataLayer.push(arguments);}
-      gtag('js', new Date());
-
-      gtag('config', 'G-X2MEN4YWXH');`}
-    </script>
         </head>
       )}
       
